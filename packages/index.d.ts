b8b0ed6f1324e7a482da4f80e57a329742f66f83
CiroLee <ciromail@126.com>
2022-07-25 04:01:13 +0800

    fix: fix type error when use globally

--- packages/index.d.ts
+++ packages/index.d.ts
@@ -1,9 +1,12 @@
+import type { App } from 'vue';
 declare const Shape: import('vue').DefineComponent<{
   type: string;
   size?: number | string | null;
   width?: number | string | null;
   height?: number | string | null;
   color?: string | null;
-}>;
+}> & { install: (app: App) => void };
 
 export { Shape };
+
+export default Shape;
